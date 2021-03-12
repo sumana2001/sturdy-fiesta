eb778e2a93f588176a9a443a66d7877126a24ab3
sumana2001 <sumana.basu2001@gmail.com>
2021-03-12 14:48:35 +0530

    hello

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+  const sayHello = ()=>{
+    console.log('Hello');
+  }
+  return(
+    <div>
+      <h1>Hello World</h1>
+      <button onClick={sayHello}>Hi</button>
     </div>
-  );
-}
-
+  )
+};
 export default App;
